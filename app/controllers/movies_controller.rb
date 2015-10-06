9ba1dc42948e0079b9f5b6fc42a9816b556eacfd
Alan <alanlim25@gmail.com>
2015-10-06 15:38:05 +0000

    Added Modal to page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,20 @@
 class MoviesController < ApplicationController
 
 	def index
-		@movie = Description.order("RANDOM()").first
+		@movie = Movie.order("RANDOM()").first
 	end
 
-	def new
-		@movie = Movie.new
+	def create
+		@movie = Movie.create(movie_params)
+		if @movie.invalid?
+			flash[:error] = 'Could not enter the data in'
+		end
+		redirect_to root_path
+	end
+
+	private
+
+	def movie_params
+		params.require(:movie).permit(:movie, :actors, :director, :description)
 	end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ActiveRecord::Base
+	validates :movie, :presence => true, :length => {:maximum => 200, :minimum => 2}
+	validates :actors, :presence => true, :length => {:maximum => 1500, :minimum => 4}
+	validates :director, :presence => true, :length => {:maximum => 100, :minimum => 2}
+	validates :description, :presence => true, :length => {:maximum => 2000, :minimum => 2}
 end
